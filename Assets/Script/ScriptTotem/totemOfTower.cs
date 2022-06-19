7bd15cbeacce6327c2a2b80136c7e018f3dc3ae3
Zarkhein <63240197+Zarkhein@users.noreply.github.com>
2022-06-19 23:41:37 +0200

    Update Totem
    Add:
    - Totem a detruire sur la map pour avancé dans le jeu (Alpha)
    Destruction, degat, drop
    [Manque] ajout de tir pour rendre les totems moins vulnérable
    
    - Futur Fix:
    - Bug avec l'information du skill
    Quand on quitte le menu avec la souris sur le skill ça laisse afficher le texte
    (quand on exit on doit mettre le setActive a false a tous les infos)

---
+++ Assets/Script/ScriptTotem/totemOfTower.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class totemOfTower : MonoBehaviour
+{
+    [Header("Stats")]
+    [SerializeField]
+    public float currentLife = 50;
+
+    [Header("Loots")]
+    [SerializeField]
+    public int dropMoney;
+
+    [Header("Dev")]
+    [SerializeField]
+    public Text textMoneyPlayer;
+    private int moneyRand;
+
+    void Start()
+    {
+        moneyRand = Random.Range(0, dropMoney);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.name == "fireball(Clone)")
+        {
+            Destroy(collision.gameObject);
+            currentLife -= playerController.instance.damage;
+            if (currentLife <= 0)
+            {
+                Destroy(gameObject);
+                playerController.instance.moneyPlayer += moneyRand;
+                textMoneyPlayer.text = playerController.instance.moneyPlayer.ToString();
+            }
+        }
+
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("Le boss touche le joueur");
+            playerController.instance.currentHealth -= 1;
+        }
+    }
+}
+
